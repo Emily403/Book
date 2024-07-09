888795cd995f62bb6bd07413eaf384c829387306
Emily403 <e.phillips@she.gdst.net>
2024-07-09 15:38:21 +0000

    woks

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-from flask import Flask, render_template 
+from flask import Flask, render_template, request 
 from replit import db
 
 
 db["Awsome"] = { "author":"Andy", "rating":"1",  "image":"https://i.pinimg.com/originals/86/14/09/861409c0a92f6777d9b07346766fa00f.gif", "Date finished":"1/1/1"}
 db["Barbaric"] = { "author":"Barry", "rating":"2", "image": "https://i.pinimg.com/736x/68/f2/4c/68f24c81b5b82bee2be4b2bdd699bd1c.jpg", "Date finished":"2/2/2"}
 db["Cool"] = { "author":"Cecil", "rating":"3", "image":"https://i.pinimg.com/736x/63/91/d4/6391d47ec03aa844555dccba7db5fabf.jpg", "Date finished":"3/3/3"}
+db["Dead"] = { "author":"Dad", "rating":"4",  "image":"https://i.pinimg.com/originals/86/14/09/861409c0a92f6777d9b07346766fa00f.gif", "Date finished":"4/4/4"}
 
 """del db["Barbatic"]"""
 
@@ -25,6 +26,31 @@ def home_func(title):
 @app.route('/form')
 def new_template():
     return render_template('Form.html')
+
+#does this work or leqve it?
+@app.route('/formafter')
+def form_after():
+    return render_template('Proof form worked.html',db=db)
+#
+
+
+#for form but why doesnt it work
+
+@app.route('/formafter', methods=['GET', 'POST'])
+def home():
+  if request.method == 'POST':
+    name = request.form['name']
+    userauthor = request.form['userauthor']
+    userstars= request.form['userstars']
+    userimage= request.form['userimage']
+    userfinished= request.form['userfinished']
+    # Process the name here (e.g., store it in a database)
+    db[name]= { "author":userauthor, "rating":userstars, "image": userimage, "datte finished":userfinished}
+    print(db["rating"])
+    print("ho")
+    return db[name]
+  else:
+    return render_template('index.html')
 
 
 
