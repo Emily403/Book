758485b5620e165c20c89f34dce2e8d316a5970d
Emily403 <e.phillips@she.gdst.net>
2024-07-10 09:20:07 +0000

    yay

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ db["Barbaric"] = { "author":"Barry", "rating":"2", "image": "https://i.pinimg.co
 db["Cool"] = { "author":"Cecil", "rating":"3", "image":"https://i.pinimg.com/736x/63/91/d4/6391d47ec03aa844555dccba7db5fabf.jpg", "df":"3/3/3"}
 db["Dead"] = { "author":"Dad", "rating":"4",  "image":"https://i.pinimg.com/originals/86/14/09/861409c0a92f6777d9b07346766fa00f.gif", "df":"4/4/4"}
 
-"""del db["H"]"""
+"""del db["Eic"]"""
 
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
     return render_template(
-        'index.html', db=db,
+        'index.html', db=db, sorteddb = sorted(db.keys(),key=str.lower,)
     )
+    #can do ,reverse = true
 
 
 @app.route("/book/<title>")
@@ -30,7 +31,7 @@ def new_template():
 #does this work or leqve it?
 @app.route('/formafter')
 def form_after():
-    return render_template('Proof form worked.html',db=db)
+    return render_template('Proof form worked.html',db=db,)
 #
 
 
@@ -40,17 +41,18 @@ print("carzy")
 @app.route('/formafter', methods=['GET', 'POST'])
 def form():
   if request.method == 'POST':
-    name = request.form['title']
+    title = request.form['title']
     userauthor = request.form['userauthor']
     userstars= request.form['userstars']
     userimage= request.form['userimage']
     userdf= request.form['userdf']
     # Process the name here (e.g., store it in a database)
-    db[name]= { "author":userauthor, "rating":userstars, "image": userimage, "df":userdf}
+    db[title]= { "author":userauthor, "rating":userstars, "image": userimage, "df":userdf}
     print("ho")
     return db[title]
   else:
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
